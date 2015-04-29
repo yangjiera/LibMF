46ec2e52f3c6fd8c78841c1df5669d49d067ce4f
yangjiera <yangjiera@gmail.com>
2015-04-29 17:59:13 +0200

    split original enmf to social nmf and collective nmf

--- tool/cross_validater.py
+++ tool/cross_validater.py
@@ -6,19 +6,19 @@ from dataset_loader import *
 from mf import *
 from evaluater import *
 
-def cross_validate(dataset, cv=True):
+def cross_validate(dataset, cv=True, filter=""):
     print 'Loading data...\n' + \
             '[warning]: users and items in test set much appear in training set, ' + \
             'otherwise the corresponding UI interaction records are removed.'
     for i in xrange(5):
-        #UI_matrix_train, UI_test, u_mapper, i_mapper = load_dataset(dataset, cv, i+1, "amsterdam")
-        UI_matrix_train, UI_test, u_mapper, i_mapper = load_dataset(dataset, cv, i+1)
+        UI_matrix_train, UI_test, u_mapper, i_mapper = load_dataset(dataset, cv, i+1, filter)
+        #UI_matrix_train, UI_test, u_mapper, i_mapper = load_dataset(dataset, cv, i+1)
         O = get_mask_matrix(UI_matrix_train)
         
         alpha = 0.01 
         beta = 0.01
-        lamb = 0.001
-        mf = Sparse_BasicMF(alpha, beta, lamb, 10, 1000, O)
+        lamb = 0.00025
+        mf = Sparse_BasicMF(alpha, beta, lamb, 100, 1000, O)
         
         print 'Decomposing...'
         U_dcp, V_dcp = mf.decomp(UI_matrix_train)
@@ -41,4 +41,5 @@ def get_mask_matrix(X):
     return O
 
 if __name__ == '__main__':
-    cross_validate('Movielens/ml-100k', True)
+    #cross_validate('Movielens/ml-100k', True)
+    cross_validate('FourCity', True, filter="amsterdam")
--- tool/exploratory_analyzer.py
+++ tool/exploratory_analyzer.py
@@ -7,5 +7,5 @@ if __name__ == '__main__':
     UI_matrix, u_mapper, i_mapper = load_dataset('FourCity', False, 0, "amsterdam")
     u_info = read_side_info('FourCity', u_mapper, 'u')
     v_info = read_side_info('FourCity', i_mapper, 'v')
-    #print UI_matrix.shape
+    print UI_matrix.shape
     
